ecce46ded683d34348c8c780ce28c43036a7ed4a
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-09 17:07:58 +0530

    Updated files

--- PYTEST DATADRIVEN USING EXCEL/testfiles/test_dataDrivenExcel.py
+++ PYTEST DATADRIVEN USING EXCEL/testfiles/test_dataDrivenExcel.py
@@ -1,30 +1,51 @@
 import pytest
-
-from selenium.webdriver.common.by import By 
-from selenium import webdriver
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
 from Utilities import excelReader
 from Utilities import logCreator
 
-@pytest.mark.parametrize("username,password",excelReader.get_data("ExcelFiles/logindata.xlsx", "login"))
 
+@pytest.mark.parametrize(
+    "username,password",
+    excelReader.get_data("ExcelFiles/logindata.xlsx", "login")
+)
 class TestLogindemo:
-    log=logCreator.log_generator()
-    def test_validlogin(self,username,password):
-        self.driver=webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.get("https://www.demoblaze.com/")
+
+    log = logCreator.log_creator()
+
+    def test_validlogin(self, username, password):
+
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://www.demoblaze.com/")
+
         self.log.info("Application Launched")
-        self.driver.find_element(By.ID,value="login2").click()
-        time.sleep(5)
-        self.driver.find_element(By.ID,value='loginusername').send_keys(username)
-        time.sleep(5)
-        self.driver.find_element(By.ID,value='loginpassword').send_keys(password)
-        self.driver.find_element(By.XPATH,value="//button[text()='Log in']").click()
-        self.log.info("Logineg successfully")
-        time.sleep(5) 
-        assert self.driver.find_element(By.ID,value='logout2').is_displayed()
-        self.log.info("Assertion got success")
-        self.driver.quit()
 
+        driver.find_element(By.ID, "login2").click()
+        time.sleep(2)
 
+        driver.find_element(By.ID, "loginusername").send_keys(username)
+        driver.find_element(By.ID, "loginpassword").send_keys(password)
+
+        driver.find_element(By.XPATH, "//button[text()='Log in']").click()
+
+        self.log.info(f"Login attempted with Username={username}")
+
+        time.sleep(5)
+
+        try:
+            logout_btn = driver.find_element(By.ID, "logout2")
+
+            if logout_btn.is_displayed():
+                self.log.info("Login Successful")
+                assert True
+
+        except NoSuchElementException:
+            self.log.error("Login Failed")
+            pytest.fail(f"Login failed for Username={username}")
+
+        finally:
+            driver.quit()
