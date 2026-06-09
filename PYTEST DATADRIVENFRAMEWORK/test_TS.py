c7ad90e0d37c2a82748157e05af8e7a4a2fbc3e0
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-09 23:46:52 +0530

    Updated

--- PYTEST DATADRIVENFRAMEWORK/test_TS.py
+++ PYTEST DATADRIVENFRAMEWORK/test_TS.py
@@ -2,53 +2,39 @@ import pytest
 from selenium.webdriver.common.by import By
 import read_config
 
-
 @pytest.mark.usefixtures("setup_and_teardown")
 class TestSearch:
 
     def test_valid_product(self):
-
         val = read_config.get_config("search term", "validterm")
-
         self.driver.find_element(By.NAME, "search").clear()
         self.driver.find_element(By.NAME, "search").send_keys(val)
-
         self.driver.find_element(
             By.XPATH,
             "//button[contains(@class,'btn-default')]"
         ).click()
-
         assert self.driver.find_element(
-            By.LINK_TEXT,
-            "HP LP3065"
+            By.LINK_TEXT, "HP LP3065"
         ).is_displayed()
 
     def test_invalid_product(self):
-
         val = read_config.get_config("search term", "invalidterm")
-
         self.driver.find_element(By.NAME, "search").clear()
         self.driver.find_element(By.NAME, "search").send_keys(val)
-
         self.driver.find_element(
             By.XPATH,
             "//button[contains(@class,'btn-default')]"
         ).click()
-
         message = self.driver.find_element(
             By.XPATH,
             "//p[contains(text(),'There is no product')]"
         ).text
-
         assert "no product" in message.lower()
 
     def test_noproduct(self):
-
         self.driver.find_element(By.NAME, "search").clear()
-
         self.driver.find_element(
             By.XPATH,
             "//button[contains(@class,'btn-default')]"
         ).click()
-
         assert "Search" in self.driver.title
