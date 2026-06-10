0deb82bc965f4386f1abd63698c1b84c9dc987bb
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-10 15:52:44 +0530

    Page factory

--- PAGE FACTORY/conftest.py
+++ PAGE FACTORY/conftest.py
@@ -1,10 +1,15 @@
 import pytest
 from selenium import webdriver
-@pytest.fixture()
+
+@pytest.fixture(scope="class")
 def setup_and_teardown(request):
+
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.get("https://tutorialsninja.com/demo/")
+
     request.cls.driver = driver
+
     yield
+
     driver.quit()
