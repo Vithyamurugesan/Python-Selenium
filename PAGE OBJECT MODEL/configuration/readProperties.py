20c9efbd4b99957be1a83096d8e7fd340e1659cb
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-10 14:32:39 +0530

    Updated the readProperties

--- PAGE OBJECT MODEL/configuration/readProperties.py
+++ PAGE OBJECT MODEL/configuration/readProperties.py
@@ -1,14 +1,18 @@
 from configparser import ConfigParser
+import os
 
 config = ConfigParser()
-config.read("configuration/config.ini")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(current_dir, "config.ini")
+config.read(config_path)
+
 class ReadConfig:
- 
+  
     def get_application_url():
         return config.get("common info", "baseURL")
-
+ 
     def get_email():
         return config.get("common info", "email")
- 
+
     def get_password():
         return config.get("common info", "password")
